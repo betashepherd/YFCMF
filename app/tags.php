e080f29a4922865c59ce046f997318b31f14c893
yangchao <yangchao@conew.com>
2017-01-19 22:32:09 +0800

    init

---
+++ app/tags.php
@@ -0,0 +1,23 @@
+<?php	return array (
+  'app_init' => 
+  array (
+  ),
+  'app_begin' => 
+  array (
+  ),
+  'module_init' => 
+  array (
+  ),
+  'action_begin' => 
+  array (
+  ),
+  'view_filter' => 
+  array (
+  ),
+  'log_write' => 
+  array (
+  ),
+  'app_end' => 
+  array (
+  ),
+);
